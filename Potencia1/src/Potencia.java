266328e541af5614b31d7723e24dabf2c7eb2dcb
ladislao <ladislao@ladislao-GE60-2PE>
2018-03-11 11:29:20 +0100

    Primer Commit

---
+++ Potencia1/src/Potencia.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Potencia 
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader lectura = new 
+		BufferedReader(new InputStreamReader(System.in));
+		int base, exp, i;
+		int resultado=1;
+		System.out.println("Indicar el valor de la base:");
+		base = Integer.parseInt(lectura.readLine());
+		System.out.println("Indicar el valor del exponente:");
+		exp = Integer.parseInt(lectura.readLine());
+		for (i=1;i<=exp;i++)
+		{
+			resultado=resultado*base;
+		}
+		System.out.println("El número "+base+" elevado a "+exp+" es igual a "+resultado);
+	}
+}
+
